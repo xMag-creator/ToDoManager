a68b52934d5767467b4c7fc3b97784ae1d1bd540
Przemysław Kobylarz <p.kobylarz@outlook.com>
2022-07-07 21:02:18 +0200

    task list model added

--- task_list/models.py
+++ task_list/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 
 
+class TaskList(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    finished = models.BooleanField(default=False)
+
